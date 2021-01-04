9988cf645e097d2081394a19116ecb8367de762a
wang <ccna8008@163.com>
2021-01-04 22:02:55 +0800

    更新tensorflow

---
+++ docs/keras.md
@@ -0,0 +1,4 @@
+---
+id: keras
+title: keras 高级神经网络API
+---
--- sidebars.js
+++ sidebars.js
@@ -5,6 +5,7 @@ module.exports = {
     Case: ['命令行终端设置代理','CLOSE_WAIT案例','精准定位问题'],
     工具: ['安装sonar','JupyterLab','python'],
 	系统: ['安装kali桌面'],
-	Test: ['doc1','doc2','doc3']
+	Test: ['doc1','doc2','doc3'],
+    Tensorflow: ['Keras','ormb']
   },
 };
